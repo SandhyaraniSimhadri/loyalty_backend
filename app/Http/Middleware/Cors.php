89ad0c05d5868074a936d1503b57fc79ad90ddb9
sandhyasimhadri <sandhyasimhadri999@gmail.com>
2024-04-15 10:58:42 +0530

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -25,6 +25,7 @@ class Cors {
         
         $response = $next($request);
         $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:4200/');
+        $response->headers->set('Access-Control-Allow-Origin', 'http://13.60.37.138/');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application', 'ip');
         return $response; 
