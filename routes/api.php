cacb26e4215d674e4993cdaa3f06dce23c986997
Sandhya Rani Simhadri <sandhyasimhadri999@gmail.com>
2025-06-27 16:49:39 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -32,10 +32,10 @@ Route::post('gameUserLogin', 'UsersController@gameUserLogin');
 Route::post('verify_user', 'UsersController@verify_user');
 
 
+    Route::get('userScore', 'UsersController@userScore');
 
 
 Route::group(['middleware' => ['cors']], function () {
-    Route::post('userScore', 'UsersController@userScore');
     Route::post('set_registration', 'UsersController@set_registration');
     Route::middleware('api_check')->post('update_user_password', 'UsersController@update_user_password');
     Route::middleware('api_check')->post('update_social_media_account', 'UsersController@update_social_media_account');
