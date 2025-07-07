b5b3b6333fe8dbb9b324c9718b4fc02da0259d26
Sandhya Rani Simhadri <sandhyasimhadri999@gmail.com>
2025-07-07 12:41:54 +0530

    changes

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,11 +25,10 @@ class UsersController extends Controller{
     public function __construct()
     {
     }
-   public function userScore(Request $request)
-{
-    return "hi";
-    return response()->json(['status' => true, 'msg' => 'controller reached']);
-}
+//    public function userScore(Request $request)
+// {
+//     return response()->json(['status' => true, 'msg' => 'controller reached']);
+// }
 
     public function  check_user(Request $request)
     {
@@ -591,7 +590,7 @@ class UsersController extends Controller{
                 return json_encode($response);
             }
         }
-       public function userScore1(Request $request)
+       public function userScore(Request $request)
 {
     $token = $request->header('Authorization');
 
